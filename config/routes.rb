7f1e0218faa41a68a6f5ea8491a6fc5d35e9e79a
Neil Pulcyn <neilpulcyn@hotmail.co.uk>
2021-11-11 17:01:36 +0000

    almost there

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, only: %i[create new show index] do
+    resources :bookmarks, only: %i[new create]
+    root to: 'lists#index'
+  end
+  resources :bookmarks, only: [:destroy]
 end
